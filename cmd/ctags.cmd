bb0e64ac6fef6c4bc4cfd2307b63542e8199a280
alen6516 <alen6516@gmail.com>
2019-08-12 19:03:57 +0800

    update ctags.cmd

--- cmd/ctags.cmd
+++ cmd/ctags.cmd
@@ -12,3 +12,5 @@ $ ctags -R .
 $ ctags -R -f ./.git/tags .
     * recursive parse child dir and generate tag file in .git dir of current dir
 
+$ ctags -R --languages=c,c++
+    * c parses c function, c++ parses macros (preprocessor)
