86fecf4b70e62b8d9513ed1f83db87b614c08ce1
alen6516 <alen6516@gmail.com>
2019-11-13 19:42:53 +0800

    update tcprewrite

--- cmd/tcprewrite.cmd
+++ cmd/tcprewrite.cmd
@@ -14,6 +14,12 @@
 --enet-dmac=DST_MAC
     rewrite dmac to DST_MAC
 
+--enet-dmac=DST_MAC_A,DST_MAC_B
+    rewrite dmac from DST_MAC_A to DST_MAC_B
+
+-e IP_A:IP_B
+    rewrite ip from IP_A to IP_B
+
 -D IP_A:IP_B
     rewrite dst ip from IP_A to IP_B
 
