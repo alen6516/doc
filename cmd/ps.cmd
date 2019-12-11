a18ba6a940912116e01a96db215bf95390adf8a0
alen6516 <alen6516@gmail.com>
2019-12-11 09:44:22 +0800

    add ps.cmd

---
+++ cmd/ps.cmd
@@ -0,0 +1,14 @@
+# Description
+
+# Overview
+
+# Options
+
+    
+# Example
+$ ps aux | grep PROCESS
+    * show pid of the process
+
+$ ps -efj | grep PROCESS
+    * show pid, ppid of the process
+
