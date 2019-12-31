a3c303b97d1565a90cfee027474e3dc1ee1156b3
alen6516 <alen6516@gmail.com>
2019-12-31 10:59:54 +0800

    add dig.cmd

---
+++ cmd/dig.cmd
@@ -0,0 +1,19 @@
+# Description
+
+# Overview
+
+# Options
+-x 
+    reverse lookup
+
++short
+    show short output
+
++trace
+    trace the nodes
+    
+# Example
+$ dig @8.8.8.8 www.msn.com
+
+$ dig -x 172.194.72.100 +short
+
