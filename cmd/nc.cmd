42fd906fef9030b913c2cfd38f286e8fa6be5d91
alen6516 <alen6516@gmail.com>
2020-01-06 18:53:24 +0800

    update nc.cmd

--- cmd/nc.cmd
+++ cmd/nc.cmd
@@ -27,3 +27,14 @@ $ echo -n "foo" | nc -u -w1 127.0.0.1 5000
     $ echo -e "$(cat http_msg_file)" | nc 127.0.0.1 80
     * example http GET msg
         * GET / HTTP/1.1\r\nHost: 127.0.0.1\r\nUser-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nIf-Modified-Since: Tue, 08 Oct 2019 14:37:29 GMT\r\nIf-None-Match: "c-594671ba63449"\r\nCache-Control: max-age=0\r\n\r\n
+
+* simple TLS server
+    $ nc -lvnp 12345
+    $ ncat -lvnp 12345 --ssl
+    * for SSL client
+        $ gnutls-lci imap.gmail.com -p 993
+        $ openssl s_client -connect imap.gmail.com:993
+        $ socat openssl:imap.gmail.com:993 stdio
+        $ socat ssl:imap.gmail.com:993 readline
+        $ ncat --ssl imap.gmail.com 993
+        $ telnet-ssl -z ssl imap.gmail.com 993
