01a01c1e66d177e6404fee23bf8e40761dd6168e
alen6516 <alen6516@gmail.com>
2020-01-09 12:21:15 +0800

    add date.cmd

---
+++ cmd/date.cmd
@@ -0,0 +1,21 @@
+# Description
+
+# Overview
+date +format
+
+# Options
+## format
+%D
+    date; same as %m/%d/%y
+%H
+    hour (00..23)
+%M
+    minute (00..59)
+%S
+    second (00..60)
+%s
+    seconds since 1970-01-01 00:00:00 UTC
+    
+    
+# Example
+date +%H:%M:%S
