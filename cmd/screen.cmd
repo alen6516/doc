c830c99fde84da1689224ffd17c7c4ae22e53e75
alen6516 <alen6516@gmail.com>
2020-01-16 09:33:30 +0800

    add screen.cmd

---
+++ cmd/screen.cmd
@@ -0,0 +1,29 @@
+# Description
+
+# Overview
+
+# Options
+-E
+    do not interpret backslash escape
+
+    
+# Example
+
+- outside screen
+    - list screen session
+        - screen -ls
+    - detach certain screen
+        - screen -d PID
+    - resume session
+        - screen -r PID
+        - screen -r
+- inside screen
+    - leading-key: `ctrl+a`
+    - create new window
+        - leading-key + c
+    - move to window by number
+        - leading-key + NUMBER
+        - detach
+            - leading-key + d
+    - show manu
+        - leader-key + <ctrl-?>
