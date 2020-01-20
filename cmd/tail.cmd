e47e810903f31875b1a4ec136e49294e93bbebee
alen6516 <alen6516@gmail.com>
2020-01-20 14:51:08 +0800

    add tail.cmd

---
+++ cmd/tail.cmd
@@ -0,0 +1,12 @@
+# Description
+
+# Overview
+
+# Options
+-f FILE
+    follow mode, keep tracing the content of output
+
+    
+# Example
+$ tail -f output.log
+    * keep tracing the content of output.log
