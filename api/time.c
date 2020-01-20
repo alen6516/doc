39c880946b2a8e249a26891f7ca5300538b63ad1
alen6516 <alen6516@gmail.com>
2020-01-20 10:07:14 +0800

    update time.c

--- api/time.c
+++ api/time.c
@@ -1,3 +1,14 @@
+#include <time.h>
+#include <stdio.h>
 
-// get the system time
-time_t now = time(NULL);
+int main () {
+    // get the system time
+    time_t now = time(NULL);
+
+    // or
+    time_t raw_time;
+    struct tm* timeinfo;
+    time(&raw_time);
+    timeinfo = localtime(&raw_time);
+    printf ( "Current local time and date: %s", asctime (timeinfo) );
+}
