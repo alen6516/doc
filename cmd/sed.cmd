0c2da10ebe6d16040f416c012692e064dafea891
alen6516 <alen6516@gmail.com>
2020-02-14 13:40:48 +0800

    add sed.cmd

---
+++ cmd/sed.cmd
@@ -0,0 +1,12 @@
+# Description
+
+# Overview
+
+# Options
+
+    
+# Example
+$ sed -i "s/old_string/new_string/g" file1 file2 file3
+    * replace "old_string" with "new_string" in file1, file2, file3
+    * if no -i, the command just output the result to cli
+
