f6d463782012abb0d404ad7534787c59c2ed97e9
alen6516 <alen6516@gmail.com>
2020-03-05 18:03:51 +0800

    update hping3

--- cmd/hping3.cmd
+++ cmd/hping3.cmd
@@ -13,6 +13,21 @@ packet maker
     * payload's size, not including any kinds of protocol header
 
 -c COUNT
+    * specify total pkt count
+
+-1
+    * using ICMP
+
+-2
+    * using UDP
+
+## Layer 1
+-I INTF
+    * specify emit interface
+
+## Layer 2
+-m MTU 
+    * specify MTU
 
 ## IP
 -a IP_addr
@@ -21,11 +36,26 @@ packet maker
 --rand-source
     * random the src IP
 
+-t TTL
+    * specify TTL
+
+-N NUM
+--id NUM
+    * specify ID of IP packet
+
+-f
+--frag
+    * fragment the data into multiple packets, default MTU is 16 bytes
+
 ## TCP/UDP
 -p PORT
     dst port
 
 -k
     don't vary src port
-    
+
+## APP Layer
+-d LEN
+    * specify data length
+
 # Example
