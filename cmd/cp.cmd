0afdd5728658c98d1d6668cb7202989c1f0aca1a
alen6516 <alen6516@gmail.com>
2020-03-09 10:51:18 +0800

    add cp

---
+++ cmd/cp.cmd
@@ -0,0 +1,22 @@
+# Description
+
+# Overview
+
+# Options
+-i
+    comfirm before overwrite
+
+-p
+    preserve the attribute like owner, group, permission, time, ... of the src file
+
+-d
+    don't deference, if the src file is a link, copy the link rather than the file pointed by the link.
+
+-R
+    use to replace -r
+
+-a
+    same to -dpR
+    
+# Example
+
