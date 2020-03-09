68203de5f0daacbcb4703c08bc097ddfaedb4e4f
alen6516 <alen6516@gmail.com>
2020-03-09 13:37:49 +0800

    update apt

--- cmd/apt.cmd
+++ cmd/apt.cmd
@@ -3,6 +3,8 @@
 # Overview
 * /etc/apt/source.list stores the source of packages, update it by apt update
 
+* before using apt build-dep, one may need to edit source.list by uncomment lines start with deb-src and do apt update
+
 # Options
     
 # Example
@@ -11,3 +13,8 @@ $ apt-cache search PACKAGE
 
 $ apt-cache show PACKAGE
     * show information of package
+
+$ apt build-dep global
+    * build the environment for the  compilation of package "global"
+    * if fail, make sure you have uncomment lines starting with deb-src in source.list, and do apt update
+    
