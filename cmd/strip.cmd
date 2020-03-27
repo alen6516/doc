0bdf69abcd8904db7d3f87163548edeee69f9bf8
alen6516 <alen6516@gmail.com>
2020-03-27 17:36:31 +0800

    add strip.cmd

---
+++ cmd/strip.cmd
@@ -0,0 +1,13 @@
+# Description
+
+# Overview
+
+# Options
+-d
+    Strip debugging symbol only
+
+    
+# Example
+* $ strip -d a.out
+    * strip debugging symbol, a.out can not be debugged line by line
+    * but symbol is still there, so still can set breakpoint to main function or else
