df94748334aa9c8d5889d5c34ce94d8e80a9c002
alen6516 <alen6516@gmail.com>
2020-06-30 20:41:29 +0800

    update objdump

--- cmd/objdump.cmd
+++ cmd/objdump.cmd
@@ -3,7 +3,20 @@
 # Overview
 
 # Options
-    
+-M MODE
+	"x86-64" or "i386" or "i8086"
+	    Select disassembly for the given architecture.
+	"intel" or "att"
+		Select between intel syntax mode and AT&T syntax mode.
+	"amd64" or "intel64"
+	    Select between AMD64 ISA and Intel64 ISA.
+
+-S
+	display source code
+
 # Example
 $ objdump --debugging my_bin
     * show debugging symbols of a binary file
+
+$ objdumo -M intel -S main.o
+    * use intel syntax, and display source code 
