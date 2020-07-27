994b49c187775369b7e6d6879dfa385f4b5add3c
alen6516 <alen6516@gmail.com>
2020-07-27 09:38:09 +0800

    update scapy

--- cmd/scapy.cmd
+++ cmd/scapy.cmd
@@ -4,6 +4,8 @@
 * use git clone https://github.com/secdev/scapy.git to obtain scapy, not apt install
     * run scapy/run_scapy
 
+* IPv6 packet refer to https://www.packetlevel.ch/html/scapy/scapyipv6.html
+
 # Options
 
     
@@ -24,6 +26,13 @@ $ p = IP(dst="8.8.8.8")/TCP(dport=80)/"GET / HTTP/1.1"
     ether.src = "00:0c:29:0a:5d:10"
     ether.dst = "00:0c:29:0a:5d:1a"
 
+	## icmpv6
+	icmpv6 = ICMPv6EchoRequest()
+	ipv6 = IPv6(dst="2001:2::162")
+	pkt = (ipv6/icmpv6)
+	sr1(pkt)
+
+	## encap
     ipv6 = IPv6()
     ip = IP()
     icmp = ICMP()
