8c6a67b69ce1648a051013231bf920a459aa7b8f
alen6516 <alen6516@gmail.com>
2020-10-26 23:56:29 +0800

    update

--- cmd/gcc.cmd
+++ cmd/gcc.cmd
@@ -10,7 +10,13 @@
     * link file (.o -> .out)
 
 -I  PATH
-    * specify header file's path
+    * specify header file's path, the search priority is higher than system header files
+
+-l library
+    Search the library named library when linking.
+
+-Ldir
+    Add directory dir to the list of directories to be searched for -l.
 
 -save-temps
     * get all intermediate files in current directory
