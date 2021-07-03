d7f09f47877c90432d3c0348269c18c4e632b801
alen6516 <alen6516@gmail.com>
2021-07-03 18:17:41 +0800

    add command

---
+++ cmd/hexedit.cmd
@@ -0,0 +1,14 @@
+# Description
+view and edit files in hexadecimal or in ASCII
+
+# Overview
+Can use it to modify pcap file.
+A similar tool on windows is Colasoft Packet Builder
+
+# Options
+
+    
+# Example
+$ hexedit tmp.pcap
+    * after moving to the target byte, press key 0~f to modify
+    * ctrl-x to save and exit
