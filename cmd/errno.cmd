7b65ff9b794acf0a22974daad4f0b1eff4ebc49e
alen6516 <alen6516@gmail.com>
2021-07-13 14:14:49 +0800

    update

---
+++ cmd/errno.cmd
@@ -0,0 +1,14 @@
+# Description
+look up errno names and descriptions
+
+# Overview
+
+# Options
+-l
+    list all
+
+-s WORD
+    search for errors whose description contails WORD (case insensitive)
+    
+# Example
+$ errno -5
