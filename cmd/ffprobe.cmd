5629ec8736965194fe06594ffa7e09b8b3942caa
alen6516 <alen6516@gmail.com>
2025-03-20 09:57:18 +0800

    update

--- cmd/ffprobe.cmd
+++ cmd/ffprobe.cmd
@@ -17,5 +17,10 @@ $ ffprobe -v error -show_streams video.mp4
     * color_transfer=smpte2084
     * color_primaries=bt2020
 
+<<<<<<< Updated upstream
 $ ffprobe -v 0 -select_streams v:0 -count_frames -show_entries stream=nb_read_frames -of csv=p=0 in.mp4
     * calculate how many frames in a video
+=======
+$ ffprobe -i video.mp4 -show_frames
+    * show each frame information including frame type (I/P/B frame)
+>>>>>>> Stashed changes
